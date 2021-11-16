5c487c35bc62b8266815e258b8851fd4e901afed
Kim Duer <kimcodesjs@gmail.com>
2021-11-15 20:07:49 -0600

    initialized firebase

--- src/index.js
+++ src/index.js
@@ -2,6 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from 'firebase/app'
+
+initializeApp({
+  apiKey: "AIzaSyDFLq2YahShmw7weq9O3ex0SjQjFid3F7c",
+  authDomain: "cra-event-budgeter.firebaseapp.com",
+  projectId: "cra-event-budgeter",
+  storageBucket: "cra-event-budgeter.appspot.com",
+  messagingSenderId: "671947328237",
+  appId: "1:671947328237:web:284a6ab6ba75c9a1589ae4",
+  measurementId: "G-NP69S1HPTL"
+})
 
 ReactDOM.render(
   <React.StrictMode>
